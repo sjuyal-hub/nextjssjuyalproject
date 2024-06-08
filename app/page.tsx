285791e6b6d9baec435b946a92dbc6ce95a0e375
sjuyal-hub <shweta.juyal@gmail.com>
2024-06-08 02:44:18 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,39 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-         <h2 className="mb-3 text-2xl font-semibold">
-            Q1. From this list, rank your 5 most favourite and 5 least favourite support tasks. Provide a brief explanation for each.
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            A1. 
-          </p>
-        <h2 className="mb-3 text-2xl font-semibold">
-            Q2. What do you want to learn or do more of at work?
-        </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <h2>2024 CSE Exercise</h2>
+    <dl>
+      <dt>From this list, rank your 5 most favourite and 5 least favourite support tasks. Provide a brief explanation for each.</dt>
+        <dd> first answer </dd>
         </div>
-      </div>
+      <dt>What do you want to learn or do more of at work?</dt>
+      <dt>Describe how you solved a challenge or technical issue that you faced in a previous role(preferably in a previous support role). How did you determine that your solution was
+successful?</dt>
+  </dl>  
+
+      
     </main>
   );
 }

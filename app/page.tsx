fc0f0862d93a4b0a4aa62031d7a5dd5e96230640
sjuyal-hub <shweta.juyal@gmail.com>
2024-06-08 02:46:22 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <dl>
       <dt>From this list, rank your 5 most favourite and 5 least favourite support tasks. Provide a brief explanation for each.</dt>
         <dd> first answer </dd>
-        </div>
+        
       <dt>What do you want to learn or do more of at work?</dt>
       <dt>Describe how you solved a challenge or technical issue that you faced in a previous role(preferably in a previous support role). How did you determine that your solution was
 successful?</dt>
